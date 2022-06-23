c7ad0d7ad69808088b6c684505481087474913c8
VoThuyLinh <volinh911@gmail.com>
2022-06-23 21:46:56 +0700

    (+) authentication pages (2 pages), dashboard pages (2 pages)

---
+++ HTML/JS/sub_header.js
@@ -0,0 +1,7 @@
+/*navbar*/
+const toggleMenu = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+toggleMenu.onclick = function() {
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
